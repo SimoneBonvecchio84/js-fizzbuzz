cf9f2a878dfd3f717d71ab5ae65351e89f41777f
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-03-27 16:53:55 +0100

    Feat: add For and If test

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// Creo ciclo
+
+for(i = 0; i <= 100; i++){
+    // test
+    console.log(i);
+    //CONDIZIONE
+    if(i % 3 === 0) {
+        // test
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        // test
+        console.log("Buzz");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        // test
+        console.log("FizzBuzz");
+    }
+}
