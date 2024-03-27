2261415e7d763ee29795ff7f089261fc20614e4f
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-03-27 18:45:54 +0100

    Feat: add content var

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 
-// Creo ciclo
+const rowElem = document.querySelector(".row"); // object | null
 
-for(i = 0; i <= 100; i++){
+// Creo ciclo
+// stringa che inizializzo per il contenuto di un elemeto
+let content = "";
+for (let i = 1; i <= 100; i++) {
+    
     // test
     console.log(i);
-    
-    //CONDIZIONE
-    if(i % 3 === 0) {
-        // test
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        // test
-        console.log("Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        // test
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
+        bgClass = "fizzBuzz";
+        content = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+        bgClass = "fizz"
+        content = "Fizz";
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+        bgClass = "buzz"
+        content = "Buzz";
+    } else {
+        console.log(i);
+        bgClass = "other"
+        content = i;
+
     }
+    //CONDIZIONE
+  
+    // INIZZIALIZZO VARIABILE
+    const boxElem = document.createElement("div"); // object
+    boxElem.innerHTML = i;
+    boxElem.innerHTML = content;
+    boxElem.classList.add("box");
+    boxElem.classList.add(bgClass);
+    
+    
+    console.log(boxElem);
+    // Inserisco boxElem nella riga
+    rowElem.append(boxElem);
+
 }
 
-// INIZZIALIZZO VARIABILE
 
-const
+
+
